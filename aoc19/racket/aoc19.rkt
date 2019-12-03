a8b9861d1cc73155b302d0c4c6005668c071cbeb
sstoltze <sstoltze@gmail.com>
2019-12-03 23:43:23 +0100

    Add 2019 day2 in racket

--- aoc19/racket/aoc19.rkt
+++ aoc19/racket/aoc19.rkt
@@ -1,8 +1,11 @@
 #lang racket/base
-(require racket/file)
+(require racket/file
+         racket/string
+         racket/match
+         racket/vector)
 
 (define day1-input
-  (file->list (string->path "day1-1.txt")
+  (file->list (string->path "../input/day1-1.txt")
               (lambda (port)
                 (let ([in (read-line port 'any)])
                   (cond [(string? in) (string->number in)]
@@ -26,3 +29,38 @@
 (define (day1-2 input)
   (for/sum ([mass input])
     (total-fuel mass)))
+
+(define (day2-1-input)
+  (string->intcode-program (file->string "../input/day2-1.txt")))
+
+(define (string->intcode-program s)
+  (list->vector (map string->number
+                     (string-split s #px"[\\s,]+"))))
+
+(define (day2-intcode-machine program)
+  (define (deref p) (vector-ref program p))
+  (let loop ([ptr 0])
+    (define opcode (deref ptr))
+    (match opcode
+      [(or 1 2)
+       (vector-set! program (deref (+ ptr 3))
+                    ((match opcode [1 +] [2 *])
+                     (deref (deref (+ ptr 1)))
+                     (deref (deref (+ ptr 2)))))
+       (loop (+ ptr 4))]
+      [99 program])))
+
+(define (day2-1 input)
+  (vector-set! input 1 12)
+  (vector-set! input 2 2)
+  (vector-ref (day2-intcode-machine input) 0))
+
+(define (day2-2 input)
+  (for/or ([noun (in-range 100)])
+    (for/or ([verb (in-range 100)])
+      (let ([v (vector-copy input)])
+        (vector-set! v 1 noun)
+        (vector-set! v 2 verb)
+        (and (= (vector-ref (day2-intcode-machine v) 0)
+                19690720)
+             (+ (* noun 100) verb))))))
