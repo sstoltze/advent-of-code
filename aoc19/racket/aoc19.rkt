c3ac157870408bbc2c544211659b04e5438b37b9
sstoltze <sstoltze@gmail.com>
2019-12-06 00:03:42 +0100

    Solve aoc19/day3 in racket

--- aoc19/racket/aoc19.rkt
+++ aoc19/racket/aoc19.rkt
@@ -4,7 +4,8 @@
          racket/set
          racket/match
          racket/list
-         racket/vector)
+         racket/vector
+         racket/function)
 
 (define day1-input
   (file->list (string->path "../input/day1-1.txt")
@@ -91,74 +92,69 @@
 (define (origin-dist p)
   (manhattan-dist p (point 0 0)))
 
-(define (day3-1-input)
+(define (day3-input)
   (map (compose (lambda (s) (string-split s ",")) string-trim) (file->lines "../input/day3-1.txt")))
 
-(define (add-wire direction length x y)
-  (case direction
-    [(#\U) (for/set ([i (in-range length)])
-             (point x (+ y i 1)))]
-    [(#\D) (for/set ([i (in-range length)])
-             (point x (- y i 1)))]
-    [(#\L) (for/set ([i (in-range length)])
-             (point (- x i 1) y))]
-    [(#\R) (for/set ([i (in-range length)])
-             (point (+ x i 1) y))]))
+(define day3-test-input
+  (map (lambda (s) (string-split s ",")) '("R8,U5,L5,D3" "U7,R6,D4,L4")))
+
+(define (move-point p dir len)
+  (match-define (point x y) p)
+  (case dir
+    [(#\U) (point x (+ y len))]
+    [(#\D) (point x (- y len))]
+    [(#\L) (point (- x len) y)]
+    [(#\R) (point (+ x len) y)]))
+
+(define (add-wire p dir len )
+  (for/list ([i (in-range len)])
+    (move-point p dir (add1 i))))
 
 (define (wire->points wire)
   (for/fold ([points (set)]
-             [x 0]
-             [y 0]
+             [p (point 0 0)]
              #:result points)
             ([w (in-list wire)])
     (define direction (string-ref w 0))
     (define length (string->number (substring w 1)))
-    (define new-points (set-union points (add-wire direction length x y)))
-    (case direction
-      [(#\U) (values new-points
-                     x (+ y length))]
-      [(#\D) (values new-points
-                     x (- y length))]
-      [(#\L) (values new-points
-                     (- x length) y)]
-      [(#\R) (values new-points
-                     (+ x length) y)])))
+    (define new-points (set-union points (list->set (add-wire p direction length))))
+    (values new-points
+            (move-point p direction length))))
 
 (define (intersect-wires w1 w2)
   (define first-wire-points (wire->points w1))
   (for/fold ([intersection (set)]
-             [x 0]
-             [y 0]
+             [p (point 0 0)]
              #:result intersection)
             ([w w2])
     (define direction (string-ref w 0))
     (define length (string->number (substring w 1)))
-    (case direction
-      [(#\U) (values (set-union
-                      intersection
-                      (set-intersect first-wire-points
-                                     (add-wire direction length x y)))
-                     x (+ y length))]
-      [(#\D) (values (set-union
-                      intersection
-                      (set-intersect first-wire-points
-                                     (add-wire direction length x y)))
-                     x (- y length))]
-      [(#\L) (values (set-union
-                      intersection
-                      (set-intersect first-wire-points
-                                     (add-wire direction length x y)))
-                     (- x length) y)]
-      [(#\R) (values (set-union
-                      intersection
-                      (set-intersect first-wire-points
-                                     (add-wire direction length x y)))
-                     (+ x length) y)])))
+    (values (set-union intersection
+                       (set-intersect first-wire-points
+                                      (list->set (add-wire p direction length))))
+            (move-point p direction length))))
 
-(define (wire-steps wire point)
-  (void))
+(define (wire-steps wire target)
+  (for/fold ([result #f]
+             [length 0]
+             [p (point 0 0)]
+             #:result result)
+            ([w wire])
+    (define direction (string-ref w 0))
+    (define wire-length (string->number (substring w 1)))
+    (if (not result)
+        (values
+         (cond
+           [(index-of (add-wire p direction wire-length) target) => (lambda (i) (+ length (add1 i)))]
+           [else #f])
+         (+ length wire-length)
+         (move-point p direction wire-length))
+        (values result length p))))
 
-(define (day3-1 input)
+(define (day3 input)
   (match-define (list w1 w2) input)
   (define intersections (intersect-wires w1 w2))
-  (argmin origin-dist (set->list intersections)))
+  (values (argmin origin-dist (set->list intersections))
+          (apply min (map (lambda (p) (+ (wire-steps w1 p)
+                                         (wire-steps w2 p)))
+                          (set->list intersections)))))
