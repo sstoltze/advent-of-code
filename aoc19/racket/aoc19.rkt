fe960d7887bce586e5446b4674a796ef602aa5fd
sstoltze <sstoltze@gmail.com>
2020-04-13 21:47:38 +0200

    Solve day 4, start day 9 in racket

--- aoc19/racket/aoc19.rkt
+++ aoc19/racket/aoc19.rkt
@@ -5,7 +5,8 @@
          racket/match
          racket/list
          racket/vector
-         racket/function)
+         racket/function
+         racket/hash)
 
 (define day1-input
   (file->list (string->path "../input/day1-1.txt")
@@ -43,68 +44,71 @@
   (list->vector (map string->number
                      (string-split s #px"[\\s,]+"))))
 
-(define (intcode-parameter program ptr modes)
+(define (intcode-parameter program ptr relative-pointer modes)
   (lambda (m [immediate #f])
     (if immediate
         (vector-ref program (+ ptr m))
         (case (hash-ref modes m 0)
+          [(2)  (vector-ref program (+ relative-pointer ptr m))]
           [(1)  (vector-ref program (+ ptr m))]
           [else (vector-ref program (vector-ref program (+ ptr m)))]))))
 
 (define (intcode-op f)
-  (lambda (program ptr [modes (make-hash)])
-    (define deref (intcode-parameter program ptr modes))
+  (lambda (program ptr relative-pointer [modes (make-hash)])
+    (define deref (intcode-parameter program ptr relative-pointer modes))
     (vector-set! program (deref 3 #t)
                  (f (deref 1)
                     (deref 2)))
-    (values program (+ ptr 4))))
+    (values program (+ ptr 4) relative-pointer)))
 
 (define intcode-1 (intcode-op +))
 (define intcode-2 (intcode-op *))
-(define (intcode-3 program ptr [modes (make-hash)])
-  (define deref (intcode-parameter program ptr modes))
+(define (intcode-3 program ptr relative-pointer [modes (make-hash)])
+  (define deref (intcode-parameter program ptr relative-pointer modes))
   (vector-set! program (deref 1 #t)
                (string->number (string-trim (read-line))))
-  (values program (+ ptr 2)))
-(define (intcode-4 program ptr [modes (make-hash)])
-  (define deref (intcode-parameter program ptr modes))
+  (values program (+ ptr 2) relative-pointer))
+(define (intcode-4 program ptr relative-pointer [modes (make-hash)])
+  (define deref (intcode-parameter program ptr relative-pointer modes))
   (println (deref 1))
-  (values program (+ ptr 2)))
-(define (intcode-5 program ptr [modes (make-hash)])
-  (define deref (intcode-parameter program ptr modes))
+  (values program (+ ptr 2) relative-pointer))
+(define (intcode-5 program ptr relative-pointer [modes (make-hash)])
+  (define deref (intcode-parameter program ptr relative-pointer modes))
   (if (not (zero? (deref 1)))
-      (values program (deref 2))
-      (values program (+ ptr 3))))
-(define (intcode-6 program ptr [modes (make-hash)])
-  (define deref (intcode-parameter program ptr modes))
+      (values program (deref 2) relative-pointer)
+      (values program (+ ptr 3) relative-pointer)))
+(define (intcode-6 program ptr relative-pointer [modes (make-hash)])
+  (define deref (intcode-parameter program ptr relative-pointer modes))
   (if (zero? (deref 1))
-      (values program (deref 2))
-      (values program (+ ptr 3))))
-(define (intcode-7 program ptr [modes (make-hash)])
-  (define deref (intcode-parameter program ptr modes))
+      (values program (deref 2) relative-pointer)
+      (values program (+ ptr 3) relative-pointer)))
+(define (intcode-7 program ptr relative-pointer [modes (make-hash)])
+  (define deref (intcode-parameter program ptr relative-pointer modes))
   (vector-set! program (deref 3 #t)
                (if (< (deref 1)
                       (deref 2))
                    1
                    0))
-  (values program (+ ptr 4)))
-(define (intcode-8 program ptr [modes (make-hash)])
-  (define deref (intcode-parameter program ptr modes))
+  (values program (+ ptr 4) relative-pointer))
+(define (intcode-8 program ptr relative-pointer [modes (make-hash)])
+  (define deref (intcode-parameter program ptr relative-pointer modes))
   (vector-set! program (deref 3 #t)
                (if (= (deref 1)
                       (deref 2))
                    1
                    0))
-  (values program (+ ptr 4)))
-(define (intcode-99 program ptr [modes (make-hash)])
-  (values eof ptr))
+  (values program (+ ptr 4) relative-pointer))
+(define (intcode-99 program ptr relative-pointer [modes (make-hash)])
+  (values eof ptr relative-pointer))
 
 (define (intcode-machine ops [debug #f])
   (lambda (input)
-    (let loop ([program input]
-               [pointer 0])
+    (let loop ([program (vector-copy input)]
+               [pointer 0]
+               [relative-pointer 0])
       (when debug
         (printf "Pointer: ~A~%" pointer)
+        (printf "Relative pointer: ~A~%" relative-pointer)
         (printf "Program: ~A~%" program))
       (define opcode (vector-ref program pointer))
       (define op     (modulo opcode 100))
@@ -116,12 +120,12 @@
       (define proc (hash-ref ops op))
       (when debug
         (printf "Proc: ~A~%~%" proc))
-      (define-values (prog ptr) (proc program pointer modes))
+      (define-values (prog ptr rel) (proc program pointer relative-pointer modes))
       (if (eof-object? prog)
           program
-          (loop prog ptr)))))
+          (loop prog ptr rel)))))
 
-(define (intcode-5-6-7-8-test)
+(define (intcode-test)
   (define machine (intcode-machine (make-hash (list (cons 1 intcode-1)
                                                     (cons 2 intcode-2)
                                                     (cons 3 intcode-3)
@@ -130,6 +134,7 @@
                                                     (cons 6 intcode-6)
                                                     (cons 7 intcode-7)
                                                     (cons 8 intcode-8)
+                                                    (cons 9 intcode-9)
                                                     (cons 99 intcode-99)))))
   (println "Checks whether input is equal to 8.")
   (define prog (string->intcode-program "3,9,8,9,10,9,4,9,99,-1,8"))
@@ -158,6 +163,9 @@
   (println "Outputs the given input:")
   (define prog7 (string->intcode-program "3,0,4,0,99"))
   (machine prog7)
+  (println "Takes no input, produces itself as output.")
+  (define prog8 (string->intcode-program "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"))
+  (machine prog8)
   )
 
 (define day2-intcode-machine (intcode-machine (make-hash (list (cons 1 intcode-1)
@@ -271,6 +279,65 @@
 
 (define (day5 input)
   (println "5-1: Enter 1 when prompted.")
-  (day5-intcode-machine (vector-copy input))
+  (day5-intcode-machine input)
   (println "5-2: Enter 5 when prompted.")
-  (day5-intcode-machine (vector-copy input)))
+  (day5-intcode-machine input))
+
+(define (day4-1-input)
+  (map string->number (string-split (string-trim (file->string "../input/day4-1.txt")) "-")))
+
+(define (six-digit? n)
+  (= (string-length (number->string n)) 6))
+
+(define (equal-adjacent? n)
+  (define ns (number->string n))
+  (for/or ([i (in-range (sub1 (string-length ns)))])
+    (define sub (substring ns i (+ i 2)))
+    (char=? (string-ref sub 0) (string-ref sub 1))))
+
+(define (equal-adjacent-2? n)
+  (define ns (number->string n))
+  (define ns-l (string-length ns))
+  (for/or ([i (in-range (sub1 ns-l))])
+    (define sub (substring ns i (+ i 2)))
+    (and (char=? (string-ref sub 0) (string-ref sub 1)))))
+
+(define (increasing-digits? n)
+  (define ns (number->string n))
+  (for/and ([i (in-range (sub1 (string-length ns)))])
+    (define sub (substring ns i (+ i 2)))
+    (char<=? (string-ref sub 0) (string-ref sub 1))))
+
+(define (day4-1-test? n)
+  (and (six-digit? n)
+       (equal-adjacent? n)
+       (increasing-digits? n)))
+
+(define (day4-1-numbers low high)
+  (for/list ([n (in-range low high)]
+             #:when (day4-1-test? n))
+    n))
+
+(define (string-group-equals s)
+  (for/fold ([result (list)]
+             [current-list (list)]
+             #:result (reverse (cons current-list result)))
+            ([c (in-string s)])
+    (if (or (empty? current-list)
+            (char=? c (first current-list)))
+        (values result                     (cons c current-list))
+        (values (cons current-list result) (list c)))))
+
+(define (two-equal-adjacent? n)
+  (not (empty?
+        (filter (lambda (x) (= x 2))
+                (map length (string-group-equals (number->string n)))))))
+
+(define (day4 input)
+  (define day4-1-output (day4-1-numbers (first input) (second input)))
+  (printf "Number of passwords: ~A~%" (length day4-1-output))
+  (printf "Number of updated passwords: ~A~%" (length (filter two-equal-adjacent? day4-1-output))))
+
+(define (intcode-9 program ptr relative-pointer [modes (make-hash)])
+  (define deref (intcode-parameter program ptr relative-pointer modes))
+  (values program (+ ptr 2) (+ relative-pointer (deref 1))))
