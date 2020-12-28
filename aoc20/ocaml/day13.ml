2301a63ad6d77b52b363b883e4a78924e68970ba
sstoltze <sstoltze@gmail.com>
2020-12-28 23:31:49 +0100

    Fix overflow errors in 2020 day 13, part 2, ocaml

--- aoc20/ocaml/day13.ml
+++ aoc20/ocaml/day13.ml
@@ -31,63 +31,40 @@ let part_one (current_time, timetable) =
      |> List.hd
      |> first_part_calc current_time
 
-type congruence = { congruent: int; modulo: int; }
+type congruence = { congruent: Z.t; modulo: Z.t; }
 
 let rec make_congruence remainder modulo =
-  if remainder < 0
-  then make_congruence (remainder + modulo) modulo
-  else { congruent = remainder mod modulo; modulo = modulo }
+  if Z.lt remainder Z.zero
+  then make_congruence (Z.add remainder modulo) modulo
+  else { congruent = Z.rem remainder modulo; modulo = modulo }
 
 let congruences_of_timetable timetable =
   let rec congruences res count = function
       [] -> res
-    | ("x" :: xs) -> congruences res (count + 1) xs
-    | x :: xs -> congruences (make_congruence (-count) (int_of_string x) :: res) (count + 1) xs
-  in congruences [] 0 timetable
+    | ("x" :: xs) -> congruences res (Z.succ count) xs
+    | x :: xs -> congruences (make_congruence (Z.neg count) (Z.of_string x) :: res) (Z.succ count) xs
+  in congruences [] Z.zero timetable
 
 let congruences = congruences_of_timetable (snd input)
 
-let extended_euclidian a b =
-  let rec extended' (old_r, r) (old_s, s) (old_t, t) =
-    if r = 0
-    then (old_s, old_t)
-    else let q = old_r / r
-         in extended' (r, old_r - q * r) (s, old_s - q * s) (t, old_t - q * t)
-  in extended' (a, b) (1, 0) (0, 1)
-
-let gcd a b = let (m1, m2) = extended_euclidian a b in m1 * a + m2 * b
-
-let rec pairwise_gcd =
-  let pairwise_gcd' a bs = List.map (fun b -> (a, b, gcd a b)) bs
-  in function
-    [] -> []
-  | x :: xs -> (pairwise_gcd' x xs) @ pairwise_gcd xs
-
 let rec chinese_remainder =
   let combine_congruences x1 x2 =
-    let (m1, m2) = extended_euclidian x1.modulo x2.modulo
-    in let new_cong = x1.congruent * x2.modulo * m2 + x2.congruent * x1.modulo * m1
-       and new_mod = x1.modulo * x2.modulo
+    let (_, m1, m2) = Z.gcdext x1.modulo x2.modulo
+    in let new_cong = Z.(x1.congruent * x2.modulo * m2 + x2.congruent * x1.modulo * m1)
+       and new_mod = Z.(x1.modulo * x2.modulo)
        in make_congruence new_cong new_mod
   in function
     [] -> raise Exit
   | cong :: [] -> cong.congruent
-  | x1 :: x2 :: xs -> chinese_remainder (combine_congruences x1 x2 :: xs)
+  | x1 :: x2 :: xs -> chinese_remainder ((combine_congruences x1 x2) :: xs)
 
-(* This is wrong *)
-let test = congruences |> chinese_remainder
-
-(* But all of these work... *)
-let test_chinese s = s |> parse_input |> snd |> congruences_of_timetable  |> chinese_remainder
-
-let test_1 = "0\n7,13,x,x,59,x,31,19" |> test_chinese
-let test_2 = "0\n17,x,13,19" |> test_chinese
-let test_3 = "0\n67,7,59,61" |> test_chinese
-let test_4 = "0\n67,x,7,59,61" |> test_chinese
-let test_5 = "0\n67,7,x,59,61" |> test_chinese
-let test_6 = "0\n1789,37,47,1889" |> test_chinese
+(* Very inefficient *)
+let part_two = congruences |> chinese_remainder
 
 let () =
   print_string "Day 13-1: Earliest wait time multiplied by bus id is ";
   input |> part_one |> print_int;
+  print_newline ();
+  print_string "Day 13-2: The earliest timestamp is ";
+  print_string (Z.to_string part_two);
   print_newline ()
