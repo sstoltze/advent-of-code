7acea8948957871e62b699a5b226e8d074a902a9
Sarah Ella Stoltze <sstoltze@gmail.com>
2023-12-04 16:47:20 +0100

    Switch flake to track emacs-config flake

--- aoc23/elixir/flake.nix
+++ aoc23/elixir/flake.nix
@@ -4,17 +4,7 @@
   # Inputs
   # https://nixos.org/manual/nix/unstable/command-ref/new-cli/nix3-flake.html#flake-inputs
   # The nixpkgs entry in the flake registry.
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils/main";
-  };
+  inputs = { emacsConfig.url = "github:sstoltze/emacs-config"; };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in {
-        devShell = pkgs.mkShell {
-          packages = with pkgs; [ elixir elixir_ls sqlite inotify-tools ];
-        };
-      });
+  outputs = { emacsConfig, ... }: emacsConfig.elixirSetup;
 }
