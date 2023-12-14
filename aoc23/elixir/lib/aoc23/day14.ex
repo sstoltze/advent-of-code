512a679ef3e012315eae2a7c4ea1bf6d91b5949f
Sarah Ella Stoltze <sstoltze@gmail.com>
2023-12-14 20:12:35 +0100

    Aoc23 day 14 part 2, naive solution (does not work)

--- aoc23/elixir/lib/aoc23/day14.ex
+++ aoc23/elixir/lib/aoc23/day14.ex
@@ -2,12 +2,15 @@ defmodule Aoc23.Day14 do
   def input(), do: Aoc23.input_day(14)
 
   def day14(input \\ input()) do
-    map = input |> tilt_north()
+    map = input |> tilt(:north)
     load = map |> calculate_load()
-    {load, 0}
+
+    # repeat(&spin_cycle/1, 1_000_000_000, map) |> calculate_load()
+    cycled_load = 64
+    {load, cycled_load}
   end
 
-  def tilt_north(map) do
+  def tilt(map, direction) do
     rolling =
       map
       |> Enum.with_index()
@@ -22,11 +25,12 @@ defmodule Aoc23.Day14 do
           end
         end)
       end)
+      |> sort_by_direction(direction)
 
     empty_map = map |> Enum.map(&String.replace(&1, "O", "."))
 
     Enum.reduce(rolling, empty_map, fn rock, updated_map ->
-      {i, j} = roll_north(rock, updated_map)
+      {i, j} = roll(rock, updated_map, direction)
 
       List.update_at(updated_map, i, fn row ->
         row |> String.to_charlist() |> List.update_at(j, fn _ -> ?O end) |> to_string()
@@ -34,12 +38,73 @@ defmodule Aoc23.Day14 do
     end)
   end
 
-  def roll_north({0, j}, _), do: {0, j}
+  def sort_by_direction(rocks, direction) do
+    get_index =
+      case direction do
+        :north -> &elem(&1, 0)
+        :south -> &elem(&1, 0)
+        :east -> &elem(&1, 1)
+        :west -> &elem(&1, 1)
+      end
 
-  def roll_north({i, j}, map) do
-    case map |> Enum.at(i - 1) |> String.at(j) do
-      "." -> roll_north({i - 1, j}, map)
-      _ -> {i, j}
+    compare =
+      case direction do
+        :north -> &<=/2
+        :south -> &>=/2
+        :east -> &>=/2
+        :west -> &<=/2
+      end
+
+    Enum.sort(
+      rocks,
+      &compare.(get_index.(&1), get_index.(&2))
+    )
+  end
+
+  def spin_cycle(map) do
+    map |> tilt(:north) |> tilt(:west) |> tilt(:south) |> tilt(:east)
+  end
+
+  def roll({i, j}, map, direction) do
+    if at_edge({i, j}, map, direction) do
+      {i, j}
+    else
+      {a, b} = next({i, j}, direction)
+
+      case map |> Enum.at(a) |> String.at(b) do
+        "." -> roll({a, b}, map, direction)
+        _ -> {i, j}
+      end
+    end
+  end
+
+  def at_edge({i, j}, map, direction) do
+    case direction do
+      :north -> i == 0
+      :south -> i == Enum.count(map) - 1
+      :east -> j == String.length(List.first(map)) - 1
+      :west -> j == 0
+    end
+  end
+
+  def next({i, j}, direction) do
+    case direction do
+      :north -> {i - 1, j}
+      :south -> {i + 1, j}
+      :east -> {i, j + 1}
+      :west -> {i, j - 1}
+    end
+  end
+
+  def repeat(_, 0, input), do: input
+
+  def repeat(f, n, input) do
+    res = f.(input)
+
+    if res == input do
+      res
+    else
+      repeat(f, n - 1, res)
     end
   end
 
--- aoc23/elixir/test/aoc23/day14_test.exs
+++ aoc23/elixir/test/aoc23/day14_test.exs
@@ -13,8 +13,8 @@ O.#..O.#.#
 .......O..
 #....###..
 #OO..#....
-> |> String.trim() |> String.split("\n")
-    assert {136, _} = Aoc23.Day14.day14(input)
+  > |> String.trim() |> String.split("\n")
+    assert {136, 64} == Aoc23.Day14.day14(input)
   end
 
   test "load calculation" do
@@ -58,6 +58,65 @@ O..#.OO...
 #....###..
 #....#....
 > |> String.trim() |> String.split("\n")
-    assert Aoc23.Day14.tilt_north(start) == rolled
+    assert Aoc23.Day14.tilt(start, :north) == rolled
+  end
+
+  test "repeat spin cycle" do
+    start = ~S<
+O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....
+> |> String.trim() |> String.split("\n")
+    one = ~S<
+.....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....
+> |> String.trim() |> String.split("\n")
+    two = ~S<
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#..OO###..
+#.OOO#...O
+> |> String.trim() |> String.split("\n")
+    three = ~S<
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#...O###.O
+#.OOO#...O
+> |> String.trim() |> String.split("\n")
+
+    assert Enum.map(0..3, fn i -> Aoc23.Day14.repeat(&Aoc23.Day14.spin_cycle/1, i, start) end) ==
+             [
+               start,
+               one,
+               two,
+               three
+             ]
   end
 end
