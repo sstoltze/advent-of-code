7e91ebbec603fe5d260c18a86a712f011def7252
Sarah Ella Stoltze <sstoltze@gmail.com>
2024-09-25 18:50:41 +0200

    Get started

--- aoc21/elixir/lib/aoc21/day16.ex
+++ aoc21/elixir/lib/aoc21/day16.ex
@@ -1,10 +1,39 @@
 defmodule Aoc21.Day16 do
-  # @related [test](aoc21/elixir/test/aoc21/day16_text.exs)
+  # @related [test](aoc21/elixir/test/aoc21/day16_test.exs)
+
+  defmodule Packet do
+    def parse(input) do
+      [packet] = input
+
+      <<version::3, type_id::3, body::bitstring>> = Base.decode16!(packet)
+
+      type =
+        case type_id do
+          4 -> :literal
+          _ -> :unknown
+        end
+
+      data = parse_body(body, type)
+
+      %{version: version, type: type_id}
+    end
+
+    def parse_body(body, :literal) do
+      nil
+    end
+
+    def parse_body(body, :unknown) do
+      nil
+    end
+  end
+
   def input do
     Aoc21.input_day(1, 1)
   end
 
-  def day16(_input \\ input()) do
+  def day16(input \\ input()) do
+    packet = Packet.parse(input)
+    dbg(packet)
     {0, 0}
   end
 end
