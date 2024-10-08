f5335e22368e49d3674294da56ff25ea10c61a90
Sarah Ella Stoltze <sstoltze@gmail.com>
2024-10-08 18:08:37 +0200

    Passing example test case

--- aoc21/elixir/lib/aoc21/day16.ex
+++ aoc21/elixir/lib/aoc21/day16.ex
@@ -2,36 +2,64 @@ defmodule Aoc21.Day16 do
   # @related [test](aoc21/elixir/test/aoc21/day16_test.exs)
 
   defmodule Packet do
-    def parse(input) when is_list(input) do
-      Enum.map(input, &parse/1)
+    def parse([packet]) do
+      parse(packet)
     end
 
     def parse(packet) do
-      <<version::3, type_id::3, body::bitstring>> = Base.decode16!(packet)
+      packet |> Base.decode16!() |> parse_bitstring()
+    end
+
+    defp parse_bitstring(packet) do
+      <<version::3, type_id::3, body::bitstring>> = packet
 
       type =
         case type_id do
           4 -> :literal
-          _ -> :unknown
+          _ -> :operator
         end
 
-      data = parse_body(body, type)
-
-      %{version: version, type: type_id, value: data}
+      body
+      |> parse_body(type)
+      |> Map.merge(%{version: version, type: type})
     end
 
-    def parse_body(_body, :unknown) do
-      nil
+    defp parse_body(<<0::1, length_in_bits::15, subpackets::bitstring>>, :operator) do
+      <<packets::size(length_in_bits), _rest::bitstring>> = subpackets
+      parse_subpackets(<<packets::size(length_in_bits)>>)
     end
 
-    def parse_body(body, :literal, current_value \\ 0) do
+    defp parse_body(<<1::1, number_of_subpackets::11, subpackets::bitstring>>, :operator) do
+      parse_subpackets(subpackets, number_of_subpackets)
+    end
+
+    defp parse_body(body, :literal, current_value \\ 0) do
       <<last::1, number::4, rest::bitstring>> = body
 
       new_value = 16 * current_value + number
 
       case last do
         1 -> parse_body(rest, :literal, new_value)
-        0 -> new_value
+        0 -> %{value: new_value, rest: rest}
+      end
+    end
+
+    defp parse_subpackets(packets, n \\ 0) do
+      packet = parse_bitstring(packets)
+
+      case {packet.rest, n} do
+        {"", _} ->
+          %{subpackets: [packet], rest: packet.rest}
+
+        {_, 1} ->
+          %{subpackets: [packet], rest: packet.rest}
+
+        _ ->
+          Map.merge(%{subpackets: [packet]}, parse_subpackets(packet.rest, n - 1), fn _key,
+                                                                                      a,
+                                                                                      b ->
+            a ++ b
+          end)
       end
     end
   end
@@ -41,8 +69,19 @@ defmodule Aoc21.Day16 do
   end
 
   def day16(input \\ input()) do
-    _packet = Packet.parse(input)
+    packet = Packet.parse(input)
 
-    {0, 0}
+    {sum_versions(packet), 0}
+  end
+
+  defp sum_versions(packet) do
+    packet.version +
+      case packet.type do
+        :literal ->
+          0
+
+        :operator ->
+          Enum.reduce(packet.subpackets, 0, fn packet, acc -> acc + sum_versions(packet) end)
+      end
   end
 end
--- aoc21/elixir/test/aoc21/day16_test.exs
+++ aoc21/elixir/test/aoc21/day16_test.exs
@@ -3,7 +3,27 @@ defmodule Aoc21.Day16Test do
   use ExUnit.Case
 
   test "parsing example" do
-    assert %{value: 2021, type: 4, version: 6} = Aoc21.Day16.Packet.parse("D2FE28")
+    assert %{value: 2021, type: :literal, version: 6} = Aoc21.Day16.Packet.parse("D2FE28")
+  end
+
+  test "operator example, type 0" do
+    assert %{
+             subpackets: [%{type: :literal, value: 10}, %{type: :literal, value: 20}],
+             type: :operator,
+             version: 1
+           } = Aoc21.Day16.Packet.parse("38006F45291200")
+  end
+
+  test "operator example, type 1" do
+    assert %{
+             subpackets: [
+               %{type: :literal, value: 1},
+               %{type: :literal, value: 2},
+               %{type: :literal, value: 3}
+             ],
+             type: :operator,
+             version: 7
+           } = Aoc21.Day16.Packet.parse("EE00D40C823060")
   end
 
   test "day16-1 example" do
