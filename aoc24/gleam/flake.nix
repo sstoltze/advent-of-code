d193f737a25fe246abfa2472b9de6515cbd5d638
Sarah Ella Stoltze <sstoltze@gmail.com>
2024-12-01 20:28:13 +0100

    Initialise aoc24 in gleam

---
+++ aoc24/gleam/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      forEachSystem = (f: nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "x86_64-linux"
+      ]
+        (system: f { inherit system; pkgs = import nixpkgs { inherit system; }; }));
+    in
+    {
+      devShells = forEachSystem ({ pkgs, ... }: {
+        default = pkgs.mkShell
+          {
+            buildInputs = with pkgs; lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.CoreServices;
+            packages = with pkgs;
+              [ gleam erlang_27 rebar3 ]
+              ++ lib.optional stdenv.isLinux inotify-tools
+              ++ lib.optional stdenv.isDarwin terminal-notifier
+              ++ lib.optional stdenv.isDarwin fswatch;
+          };
+      });
+    };
+}
