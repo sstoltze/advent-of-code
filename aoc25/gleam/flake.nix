54e0028e8a98b9f54faf0eb29728dc317896cfb6
Sarah Ella Stoltze <sstoltze@gmail.com>
2025-12-16 09:53:00 +0100

    Fix gleam flake.nix for macs

--- aoc25/gleam/flake.nix
+++ aoc25/gleam/flake.nix
@@ -5,25 +5,42 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      forEachSystem = (f: nixpkgs.lib.genAttrs [
-        "aarch64-darwin"
-        "x86_64-linux"
-      ]
-        (system: f { inherit system; pkgs = import nixpkgs { inherit system; }; }));
+      forEachSystem = (
+        f:
+        nixpkgs.lib.genAttrs
+          [
+            "aarch64-darwin"
+            "x86_64-linux"
+          ]
+          (
+            system:
+            f {
+              inherit system;
+              pkgs = import nixpkgs { inherit system; };
+            }
+          )
+      );
     in
     {
-      devShells = forEachSystem ({ pkgs, ... }: {
-        default = pkgs.mkShell
-          {
-            buildInputs = with pkgs; lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.CoreServices;
-            packages = with pkgs;
-              [ gleam erlang_27 rebar3 ]
+      devShells = forEachSystem (
+        { pkgs, ... }:
+        {
+          default = pkgs.mkShell {
+            packages =
+              with pkgs;
+              [
+                gleam
+                erlang_27
+                rebar3
+              ]
               ++ lib.optional stdenv.isLinux inotify-tools
               ++ lib.optional stdenv.isDarwin terminal-notifier
               ++ lib.optional stdenv.isDarwin fswatch;
           };
-      });
+        }
+      );
     };
 }
